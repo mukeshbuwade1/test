8a96f41be91ee50d284aad673e555a7dd3e3d74c
Mukesh Buwade <mukesh@newrise.in>
2023-06-02 17:43:04 +0530

    a

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View style={{
+      backgroundColor:"#0f0",
+      flex:1
+    }}>
+     
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
